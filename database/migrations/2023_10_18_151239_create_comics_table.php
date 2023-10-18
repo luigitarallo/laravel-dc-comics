7fc43e2c1dcc787b3c1635505b9f8227c52fc54d
luigitarallo <tarallo.gigi@gmail.com>
2023-10-18 20:53:21 +0200

    edit migrate data type

--- database/migrations/2023_10_18_151239_create_comics_table.php
+++ database/migrations/2023_10_18_151239_create_comics_table.php
@@ -14,13 +14,13 @@ return new class extends Migration {
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 60);
+            $table->string('title', 50);
             $table->text('description');
             $table->string('thumb', 500);
-            $table->char('price', 6);
-            $table->string('series', 30);
+            $table->char('price', 8);
+            $table->string('series', 50);
             $table->date('sale_date');
-            $table->string('type', 10);
+            $table->string('type', 20);
             $table->timestamps();
         });
     }
