6a7f11679f760a543912531eeab86bafd4076361
luigitarallo <tarallo.gigi@gmail.com>
2023-10-18 23:28:35 +0200

    feat: add return view

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -15,6 +15,7 @@ class ComicController extends Controller
     public function index()
     {
         $comics = Comic::all();
+        return view('comics.index', compact('comics'));
     }
 
     /**
@@ -44,9 +45,9 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Comic $comic)
     {
-        //
+        return view('comics.show', compact('comic'));
     }
 
     /**
