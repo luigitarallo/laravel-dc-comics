be956bfc2acb5b961928c343efaf187899ea2e72
luigitarallo <tarallo.gigi@gmail.com>
2023-10-19 09:24:27 +0200

    add comment to comment

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -10,7 +10,7 @@ class ComicController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * *@return \Illuminate\Http\Response
      */
     public function index()
     {
@@ -21,7 +21,7 @@ class ComicController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * *@return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -32,7 +32,7 @@ class ComicController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * *@return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
@@ -47,7 +47,7 @@ class ComicController extends Controller
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * *@return \Illuminate\Http\Response
      */
     public function show(Comic $comic)
     {
@@ -58,7 +58,7 @@ class ComicController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * *@return \Illuminate\Http\Response
      */
     public function edit($id)
     {
@@ -70,7 +70,7 @@ class ComicController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * *@return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
@@ -81,7 +81,7 @@ class ComicController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * *@return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
